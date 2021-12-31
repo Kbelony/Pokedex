4bcd671419c83015ae82a59d406e30f5163b8566
Kalvin Belony--Sagbo <kalvin.sgb@outlook.com>
2021-12-31 16:07:53 +0100

    init routing

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.modules';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+  ]
+})
+export class PagesModule { }
