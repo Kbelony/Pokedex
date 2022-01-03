80bcfa884b45c4adf44cd25020cfc69823c49bfa
Kalvin Belony--Sagbo <kalvin.sgb@outlook.com>
2022-01-03 10:29:08 +0100

    adding logo

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.modules';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { DetailsComponent } from './details/details.component';
   imports: [
     CommonModule,
     RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
