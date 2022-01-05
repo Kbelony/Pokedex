b488215badb655f108eb7b6ed2d844d3c7d879a5
Kalvin Belony--Sagbo <kalvin.sgb@outlook.com>
2022-01-05 19:04:24 +0100

    show list pokemon + link api done

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -18,15 +18,20 @@ export class PokeApiService {
       tap(res => res),
       tap(res => {
         res.results.map( (resPokemons: any) => {
-          this.http.get<any>(resPokemons.url).pipe(
-            map(
-              res => {
-                resPokemons.status = res;
-              }
-            )
+
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
           )
+
         })
       })
+    )
+  }
+  public apiGetPokemons( url : string ):Observable<any>{
+    return this.http.get<any>(url).pipe(
+      map(
+        res => res
+      )
     )
   }
 }
